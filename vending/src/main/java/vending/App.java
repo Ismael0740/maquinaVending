33d2190834c6edfff02357b283f6072222bb164d
aml360 <aml360esp@gmail.com>
2021-04-15 23:08:54 +0200

    mvn init

---
+++ vending/src/main/java/vending/App.java
@@ -0,0 +1,13 @@
+package vending;
+
+public final class App {
+
+	/**
+	 * Says hello to the world.
+	 *
+	 * @param args The arguments of the program.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
